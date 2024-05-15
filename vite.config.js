297c884bc58d46828af787de6a53dedbd2d9c4a5
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-05-15 14:03:49 +0100

    updated package

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+import { resolve } from "path";
 import htmlPurge from "vite-plugin-html-purgecss";
+import { defineConfig } from "vite";
 
-export default {
+export default defineConfig({
   plugins: [htmlPurge()],
-};
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, "src/index.html"),
+    },
+  },
+});
